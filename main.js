cb059ef75c0e8a7c8675135283042f241293e6f0
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2026-01-06 10:28:28 -0300

    ubi

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3114.2014559866243!2d-62.345369299999994!3d-38.690216500000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95edbbd2a495cdad%3A0xe387e225924c9b7b!2sPampa%20Central%20%26%20Casilda%20Igarzabal%2C%20B8000%20Bah%C3%ADa%20Blanca%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1730909133930!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!4v1767706065800!6m8!1m7!1sc_kZvBCsPGVr0I8EfxGTmA!2m2!1d-33.16996649464541!2d-62.76954414609678!3f168.49!4f-0.15000000000000568!5f2.0304332390489375" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
